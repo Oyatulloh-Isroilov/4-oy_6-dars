bf65ada64e7b4ea4a45b431b2516581855dc0d8f
Oyatulloh-Isroilov <oyatullohisroilov5@gmail.com>
2023-10-18 22:19:16 +0500

    bitmagan proekt

---
+++ app.js
@@ -0,0 +1,36 @@
+// let N = +prompt("sonni kiriting");
+// let arr = [i];
+
+// for (let i = 0; i <= N; i++) {
+//   if (i * N == 1) {
+//     console.log(i + " ta 0 bor");
+//   } else {
+//     if (i % 2 == 1) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// Poyezdga chipta
+
+// let tick = +prompt("chiptani raqamini kiriting");
+
+// let arr1 = [2, 3, 4, 5, 6, 7];
+// let arr2 = [8, 9];
+// let arr3 = [1, 10];
+
+// if ((tick = arr1)) {
+//   console.log("Econom 105K");
+// } else if ((tick = arr2)) {
+//   console.log("Biznes 140K");
+// } else if ((tick = arr3)) {
+//   console.log("VIP 210K");
+// } else {
+//   console.log("bunday vagon yo'q");
+// }
+
+// let N = +prompt("talabalar sonini kiriting");
+// let StudentNote = +prompt("talabalarni daftar raqamini kiriting");
+
+// let arr = [N];
+// let sum = 0
